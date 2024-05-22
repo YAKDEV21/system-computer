fabd16f194e84e478457968cb46fc5a185f9617d
YAKDEV21 <yakbagusdev@gmail.com>
2024-05-22 14:35:15 +0700

    Add files via upload

---
+++ tugasakhir/fungsi.php
@@ -0,0 +1,71 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "data_siswa";
+
+$koneksi    = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
+}
+$nim        = "";
+$nama       = "";
+$sukses     = "";
+$error      = "";
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if($op == 'delete'){
+    $id         = $_GET['id'];
+    $sql1       = "delete from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil hapus data";
+    }else{
+        $error  = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id         = $_GET['id'];
+    $sql1       = "select * from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $nim        = $r1['nim'];
+    $nama       = $r1['nama'];
+   
+
+    if ($nim == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $nim        = $_POST['nim'];
+    $nama       = $_POST['nama'];
+   
+
+    if ($nim && $nama && $alamat && $fakultas) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update mahasiswa set nim = '$nim',nama='$nama',alamat = '$alamat',fakultas='$fakultas' where id = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } else { //untuk insert
+            $sql1   = "insert into mahasiswa(nim,nama,alamat,fakultas) values ('$nim','$nama','$alamat','$fakultas')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
+?>
---
+++ tugasakhir/tentang.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Company Profile</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+
+    <link rel="stylesheet" href="Alat.css" />
+  </head>
+  <body>
+    <!-- navigasi -->
+<nav
+class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top"
+>
+<div class="container">
+  <a class="navbar-brand" href="#">MEDIA BELAJARKU </a>
+  <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgU3aqKQBOdsOVZhPqzzchk7yVWtd9_YiUjKg94Iv_6Uwau0U-TG0YF3oO2IkN3dQnpFUHfWkX98jpEvw75oDkKahsKxic9Rz9FGmIra1hu_4IMl5AlArwVKCoIAls8euPFECGoxz3KwuT6MN_Ppez9D9dHHWL2vCaAY1IX9GAXrD3sqEt0jNm2zara/s16000-rw/logo%20BACKGROUND%20HITAM%20Merdeka%20belajar%20merdeka%20mengajar%20format%20cdr%20jpg%20png%20ai%20pdf.png" alt="Logo" width="60" height="75" class="d-inline-block align-text-top">
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-bs-toggle="collapse"
+    data-bs-target="#navbarText"
+    aria-controls="navbarText"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+      
+    </a>
+  </div>
+</nav>
+<!-- Client -->
+<div class="container-fluid client pt-5 pb-5">
+    <div class="container text-center">
+      <div class="row pt-4 gx-4 gy-4">
+        <div class="col">
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-256/microsoft-28-761688.png"
+            />
+      
+             <!-- tim -->
+    <div class="container-fluid pt-5 pb-5 bg-light">
+      <div class="container text-center">
+        <h2 class="display-3" id="staff">Tim Kami</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et deleniti
+          quas at magni, iusto voluptates neque corrupti dolorum! Repellat,
+          quod.
+        </p>
+        <div class="row pt-4 gx-4 gy-4">
+          <div class="col-md-4 text-center tim">
+            <img
+              src="https://s3.amazonaws.com/cms-assets.tutsplus.com/uploads/users/810/profiles/19338/profileImage/profile-square-extra-small.png"
+              class="rounded-circle mb-3"
+            />
+            <h4>John Doe</h4>
+            <p>Web Designer</p>
+            <p>
+              <a href="" class="social"><i class="fab fa-twitter"></i></a>
+              <a href="" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="" class="social"><i class="fab fa-linkedin-in"></i></a>
+            </p>
+          </div>
+          <div class="col-md-4 text-center tim">
+            <img
+              src="http://gokubi.com/wp-content/uploads/2013/10/Steve-Andersen-Headshot-square1.jpeg"
+              class="rounded-circle mb-3"
+            />
+            <h4>Michael Dell</h4>
+            <p>Data Scientist</p>
+            <p>
+              <a href="" class="social"><i class="fab fa-twitter"></i></a>
+              <a href="" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="" class="social"><i class="fab fa-linkedin-in"></i></a>
+            </p>
+          </div>
+          <div class="col-md-4 text-center tim">
+            <img
+              src="https://www.kingrosales.com/wp-content/uploads/2018/05/king-rosales-profile-photo-square.jpg"
+              class="rounded-circle mb-3"
+            />
+            <h4>Paul</h4>
+            <p>Network Engineer</p>
+            <p>
+              <a href="" class="social"><i class="fab fa-twitter"></i></a>
+              <a href="" class="social"><i class="fab fa-facebook-f"></i></a>
+              <a href="" class="social"><i class="fab fa-linkedin-in"></i></a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Client -->
+    <div class="container-fluid client pt-5 pb-5">
+      <div class="container text-center">
+        <div class="row pt-4 gx-4 gy-4">
+          <div class="col">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/microsoft-28-761688.png"
+            />
+          </div>
+          <div class="col">
+            <img
+              src="https://cdn3.iconfinder.com/data/icons/glypho-social-and-other-logos/64/logo-facebook-512.png"
+            />
+          </div>
+          <div class="col">
+            <img src="https://image.flaticon.com/icons/png/512/61/61109.png" />
+          </div>
+          <div class="col">
+            <img
+              src="https://i.pinimg.com/originals/20/1d/17/201d17590b3a7bc8939ca37e577bbbd8.png"
+            />
+          </div>
+          <div class="col">
+            <img
+              src="https://www.ictmagazine.nl/wp-content/uploads/2020/10/ibm-720x340-1.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
